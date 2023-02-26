d55d0c566bc7dc5502503118e21c50f92efa47b9
ruchisharma12 <57267610+ruchisharma12@users.noreply.github.com>
2023-02-26 12:45:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+
+import WorkExp from './component/WorkExp/WorkExp';
+
+import Education from './component/Education/Education';
+
+
+
+
+function App() {
+  return (
+    <div>
+
+      <Education />
+      {/* <WorkExp /> */}
+
+    </div>
+
+  );
+}
+
+export default App;
